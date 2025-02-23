00f18a4b97428f97b0087e5b2ac92d784769d3df
mdkubit <mdkubit@gmail.com>
2025-02-23 17:02:20 -0500

    Update index.tsx

--- src/components/sections/HeroSection/index.tsx
+++ src/components/sections/HeroSection/index.tsx
@@ -53,7 +53,8 @@ function HeroBody(props: HeroSection) {
                         animate={{ opacity: 1, y: 0, scale: [1, 1.03, 1] }}
                         transition={{ duration: 1.2, ease: "easeOut" }}
                         whileHover={{ scale: 1.05 }}
-                        {...(classNames('h1', styles.title ? mapStyles(styles.title) : null) && { className: classNames('h1', styles.title ? mapStyles(styles.title) : null) })}
+                        className={classNames('h1', styles.title ? mapStyles(styles.title) : null) as string}
+
                     >
                         {title.split('').map((char, index) => (
                             <motion.span
